bfa1ec4282fd9d6c815e4049a8d4e6e94b0e35f8
MaximKuzakov <66713154+MaximKuzakov@users.noreply.github.com>
2020-06-12 16:25:51 +0800

    Добавление методов нового класса ЛР2

--- Program.cpp
+++ Program.cpp
@@ -22,6 +22,15 @@ void fractions::Out(ofstream& ofst)
     ofst << "It is a Fraction: " << x << "/" << y << endl;
     cout << "It is a Fraction: " << x << "/" << y << endl;
 }
+void polar_coordinates::InData(ifstream& ifst)
+{
+    ifst >> angle >> distance;
+}
+void polar_coordinates::Out(ofstream& ofst)
+{
+    ofst << "These are polar coordinates: " << angle << " (angle), " << distance << " (distance)" << endl;
+    cout << "These are polar coordinates: " << angle << " (angle), " << distance << " (distance)" << endl;
+}
 shape* shape::In(ifstream& ifst) {
     shape* sp;
     int k;
@@ -33,22 +42,25 @@ shape* shape::In(ifstream& ifst) {
     case 2:
         sp = new fractions;
         break;
+    case 3:
+        sp = new polar_coordinates;
+        break;
     default:
         return 0;
     }
     sp->InData(ifst);
     return sp;
 }
-// Инициализация контейнера
+// Г€Г­ГЁГ¶ГЁГ Г«ГЁГ§Г Г¶ГЁГї ГЄГ®Г­ГІГҐГ©Г­ГҐГ°Г 
 container::container() : len(0) { }
-// Очистка контейнера от элементов (освобождение памяти)
+// ГЋГ·ГЁГ±ГІГЄГ  ГЄГ®Г­ГІГҐГ©Г­ГҐГ°Г  Г®ГІ ГЅГ«ГҐГ¬ГҐГ­ГІГ®Гў (Г®Г±ГўГ®ГЎГ®Г¦Г¤ГҐГ­ГЁГҐ ГЇГ Г¬ГїГІГЁ)
 void container::Clear()
 {
     for (int i = 0; i < len; i++)
         delete cont[i];
     len = 0;
 }
-// Ввод содержимого контейнера из указанного потока
+// Г‚ГўГ®Г¤ Г±Г®Г¤ГҐГ°Г¦ГЁГ¬Г®ГЈГ® ГЄГ®Г­ГІГҐГ©Г­ГҐГ°Г  ГЁГ§ ГіГЄГ Г§Г Г­Г­Г®ГЈГ® ГЇГ®ГІГ®ГЄГ 
 void container::In(ifstream& ifst)
 {
 
@@ -65,7 +77,7 @@ void container::In(ifstream& ifst)
         }
     }
 }
-// Вывод содержимого контейнера в указанный поток
+// Г‚Г»ГўГ®Г¤ Г±Г®Г¤ГҐГ°Г¦ГЁГ¬Г®ГЈГ® ГЄГ®Г­ГІГҐГ©Г­ГҐГ°Г  Гў ГіГЄГ Г§Г Г­Г­Г»Г© ГЇГ®ГІГ®ГЄ
 void container::Out(ofstream& ofst)
 {
     ofst << "Container contains " << len << " elements." << endl;
